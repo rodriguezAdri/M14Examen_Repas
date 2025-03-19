a6b1d355f290fb3a009babf15cadc12fe16ed2d0
adrianRodriguez77 <adrian.rodriguez.vegas.7e6@itb.cat>
2025-03-19 18:15:41 +0100

    feat: Add functionality to fetch and display embassament data

--- repas/src/composables/useEmbassaments.js
+++ repas/src/composables/useEmbassaments.js
@@ -1,16 +1,19 @@
 import { ref } from 'vue';
 export function useEmbassaments() {
   const embassaments = ref([]);
+
   const fetchEmbassaments = async () => {
     const res = await fetch('https://analisi.transparenciacatalunya.cat/resource/gn9e-3qhr.json');
     const data = await res.json();
     const nomsUnics = [...new Set(data.map(e => e.estaci))];
     embassaments.value = nomsUnics;
   };
+  
   const fetchDetallEmbassament = async (nom) => {
     const url = `https://analisi.transparenciacatalunya.cat/resource/gn9e-3qhr.json?$query=SELECT%20dia,estaci,nivell_absolut,percentatge_volum_embassat,volum_embassat%20WHERE%20estaci=%27${nom}%27%20ORDER%20BY%20dia%20DESC%20LIMIT%20100`;
     const res = await fetch(url);
     return await res.json();
   };
+  
   return { embassaments, fetchEmbassaments, fetchDetallEmbassament };
 }
